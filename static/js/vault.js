cf0496bbff57277fd9a563aa08414a628beec385
Zane Barker <zane.barker@gmail.com>
2021-10-11 12:57:02 +0900

    Wrote JS to make the keypad write inside the vault screens.

--- static/js/vault.js
+++ static/js/vault.js
@@ -1,4 +1,6 @@
 let answers = [];
+let selectedVaultScreen = null;
+let selectedVaultScreenDiv = null;
 
 function getVaultSvg() {
     $("#vault-svg-div").load("/static/img/vault.svg");
@@ -26,7 +28,55 @@ function getAnswersFromUrl() {
     }
 }
 
+function keypadCallback(event) {
+    if (selectedVaultScreen == null) {
+        return;
+    }
+
+    if (event.target.attributes["data-value"].nodeValue == "CLR") {
+        selectedVaultScreenDiv.textContent = "";
+    }
+    else if (selectedVaultScreenDiv.textContent.length < 4) {
+        selectedVaultScreenDiv.textContent += event.target.attributes["data-value"].nodeValue; 
+    }
+}
+
+function setupKeypadCallbacks() {
+    document.getElementById('keypad-div0').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div1').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div2').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div3').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div4').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div5').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div6').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div7').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div8').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-div9').addEventListener('click', keypadCallback);
+    document.getElementById('keypad-divCLR').addEventListener('click', keypadCallback);
+}
+
+function vaultScreenCallback(event) {
+    if (selectedVaultScreen != null) {
+        selectedVaultScreen.style.strokeWidth = 1;
+    }
+    console.log(event.target.attributes["data-svg-id"].nodeValue);
+    selectedVaultScreen = document.getElementById(event.target.attributes["data-svg-id"].nodeValue);
+    selectedVaultScreenDiv = event.target;
+    selectedVaultScreen.style.strokeWidth = 3;
+}
+
+function setupVaultScreenCallbacks() {
+    document.getElementById('vault-screen-div2').addEventListener('click', vaultScreenCallback);
+    document.getElementById('vault-screen-div4').addEventListener('click', vaultScreenCallback);
+    document.getElementById('vault-screen-div6').addEventListener('click', vaultScreenCallback);
+    document.getElementById('vault-screen-div8').addEventListener('click', vaultScreenCallback);
+    document.getElementById('vault-screen-div10').addEventListener('click', vaultScreenCallback);
+    document.getElementById('vault-screen-div12').addEventListener('click', vaultScreenCallback);
+}
+
 $(document).ready(function() {
     getAnswersFromUrl();
     getVaultSvg();
+    setupKeypadCallbacks();
+    setupVaultScreenCallbacks();
 });
